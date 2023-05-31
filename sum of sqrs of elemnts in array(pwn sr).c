4225396466ccb3b2bd3d83f64d7574359671bd18
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2023-05-31 23:01:46 +0530

    Add files via upload

---
+++ sum of sqrs of elemnts in array(pwn sr).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,s=0;
+	scanf("%d",&n);
+	int a[i];
+	for(i=0;i<n;i++) {
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++) {
+		s+=a[i]*a[i];
+	}
+	printf("%d",s);
+}
